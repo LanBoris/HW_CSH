411e4cd082e7a63a884f6f715333a65a30ecead0
Boris Lanovenko <blanovenko@gmail.com>
2023-02-11 14:43:22 +0300

    Program 1

--- program_1/Program.cs
+++ program_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №2.
+// Программа принимает на вход два числа, и показывает, какое из них большее, а какое меньшее.
+
+Console.Write("Введите первое число а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+  Console.WriteLine("Числа равны.");
+}
+else
+{
+  if (a > b)
+  {
+    Console.WriteLine("Число а = " + a + " большее. " + "Число b = " + b + " меньшее. ");
+  }
+  else
+  {
+    Console.WriteLine("Число a = " + a + " меньшее. " + "Число b = " + b + " большее. ");
+  }
+}
