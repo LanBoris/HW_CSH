2e9702ec51d2c0876a32fe4b38dc9a51284b20bf
Boris Lanovenko <blanovenko@gmail.com>
2023-02-11 15:37:14 +0300

    Program 4

--- program_4/Program.cs
+++ program_4/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача №8.
+// Программа показывает все чётые числа в интервале от 1 до N.
+
+Console.Clear();
+Console.Write("Введите первое число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int cnt = 1;
+
+if (N == 1)
+{
+  Console.WriteLine("В данном интервале нет чётных чисел.");
+}
+else
+{
+  Console.WriteLine("Четные числа в интервале от 1 до " + N + ".");
+  while (cnt < N)
+  {
+    if (cnt % 2 == 0)
+    {
+      Console.Write(cnt + " ");
+    }
+    cnt++;
+  }
+}
