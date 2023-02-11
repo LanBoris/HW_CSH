d673cc86f42fd3a68bf43fc71bc8694da707c953
Boris Lanovenko <blanovenko@gmail.com>
2023-02-11 15:08:35 +0300

    Program 2

--- program_2/Program.cs
+++ program_2/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №4.
+// Программа принимает на вход 3 числа, и определяет максимальное из них.
+
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+  if (b == c)
+  {
+    Console.WriteLine("Числа равны.");
+  }
+  else
+  {
+    if (a > b)
+    {
+      if (a > c)
+      {
+        Console.WriteLine("Число a = " + a + " является максимальным.");
+      }
+    }
+    if (b > a)
+    {
+      if (b > c)
+      {
+        Console.WriteLine("Число b = " + b + " является максимальным.");
+      }
+    }
+    if (c > a)
+    {
+      if (c > b)
+      {
+        Console.WriteLine("Число c = " + c + " является максимальным.");
+      }
+    }
+  }
+}
+else
+{
+  if (a > b)
+  {
+    if (a > c)
+    {
+      Console.WriteLine("Число a = " + a + " является максимальным.");
+    }
+  }
+  if (b > a)
+  {
+    if (b > c)
+    {
+      Console.WriteLine("Число b = " + b + " является максимальным.");
+    }
+  }
+  if (c > a)
+  {
+    if (c > b)
+    {
+      Console.WriteLine("Число c = " + c + " является максимальным.");
+    }
+  }
+}
