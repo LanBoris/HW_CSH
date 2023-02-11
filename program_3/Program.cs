bbe6498866241bd0199ef6bad6cdd4ab129b2fcd
Boris Lanovenko <blanovenko@gmail.com>
2023-02-11 15:16:02 +0300

    Program 3

--- program_3/Program.cs
+++ program_3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Задача №6.
+// Программа проверяет является ли число чётным.
+
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+  Console.WriteLine("Число a = " + a + " является четным.");
+}
+else
+{
+  Console.WriteLine("Число a = " + a + " является нечётным.");
+}
