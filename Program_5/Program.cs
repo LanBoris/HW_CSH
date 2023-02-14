66e2f57cbdc729bb6ad373a7eabec484a2182769
Boris Lanovenko <blanovenko@gmail.com>
2023-02-15 01:38:26 +0300

    Create program 5 solution

--- Program_5/Program.cs
+++ Program_5/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 10: 
+//  Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите число = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+  int result = number % 100 / 10;
+  Console.WriteLine($"Второй цифрой числа {number} является цифра: {result}.");
+}
+else
+{
+  Console.WriteLine("Число не является трехзначным.");
+}
