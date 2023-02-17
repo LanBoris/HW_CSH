357c364d740c5510f821453d63286732e2b8e899
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 02:10:38 +0300

    Changes in program08

--- program_08/Program.cs
+++ program_08/Program.cs
@@ -7,18 +7,18 @@ Console.Clear();
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number >= 10000 && number <= 99999)
+while (number < 10000 || number > 99999)
 {
-  if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
-  {
-    Console.WriteLine($"Число {number} является палиндромом.");
-  }
-  else
-  {
-    Console.WriteLine($"Число {number} не является палиндромом.");
-  }
+  Console.WriteLine($"Число {number} не является пятизначным или оно отрицательное или равно 0!");
+  Console.Write("Введите новое число: ");
+  number = Convert.ToInt32(Console.ReadLine());
+}
+
+if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+{
+  Console.WriteLine("\n" + $"Число {number} является палиндромом.");
 }
 else
 {
-  Console.WriteLine($"Число {number} не является пятизначным!");
+  Console.WriteLine("\n" + $"Число {number} не является палиндромом.");
 }
