a5e5d7aaf0124d3c2468237ca98f905d889ef6cc
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 00:05:54 +0300

    Create program 8 (решение задачи №19)

--- program_8/Program.cs
+++ program_8/Program.cs
@@ -3,3 +3,22 @@
 // пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет; 12821 -> да; 23432 -> да
 
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number <= 99999)
+{
+  if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+  {
+    Console.WriteLine($"Число {number} является палиндромом.");
+  }
+  else
+  {
+    Console.WriteLine($"Число {number} не является палиндромом.");
+  }
+}
+else
+{
+  Console.WriteLine($"Число {number} не является пятизначным!");
+}
