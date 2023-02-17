ce5d3a541b0aea6484ad2ca987e90c6e4ff271a7
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 00:38:55 +0300

    Create program 9 (решение задачи №21)

--- program_9/Program.cs
+++ program_9/Program.cs
@@ -5,3 +5,21 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
+Console.Write("Введите координату точки A по оси x: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки A по оси y: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки A по оси z: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\n" + "Введите координату точки B по оси x: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки B по оси y: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки B по оси z: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n" + $"Расстояние между точками равняется {result:f2}");
+Console.ResetColor();
