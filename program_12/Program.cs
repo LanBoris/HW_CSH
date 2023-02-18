3d924be43ee3a3e37182061746a9e0b7011de36b
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 22:50:50 +0300

    Create program 12 (решение доп.задания №2)

--- program_12/Program.cs
+++ program_12/Program.cs
@@ -5,3 +5,31 @@
 // следует вывести – «Число интересное» иначе «Число неинтересное».
 // -> 945 - число интересное, средняя цифра - 4, разница: 9 - 5 = 4
 
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number <= 0)
+{
+  Console.Write("Вы ввели отрицательное число или ноль.\n" + "Введите новое число: ");
+  number = Convert.ToInt32(Console.ReadLine());
+}
+string stringNumber = number.ToString();
+char[] sN = stringNumber.ToCharArray();
+if (sN.Length % 2 == 1)
+{
+  char cD = sN[sN.Length / 2];
+  int centerDigit = (int)Char.GetNumericValue(cD);
+  char fD = sN[0];
+  int firstDigit = (int)Char.GetNumericValue(fD);
+  char lD = sN[sN.Length - 1];
+  int lastDigit = (int)Char.GetNumericValue(lD);
+  int diff = firstDigit - lastDigit;
+  if (centerDigit == diff)
+  {
+    Console.WriteLine("\n" + $"Число {number} интересное.");
+  }
+}
+else
+{
+  Console.WriteLine("\n" + $"Число {number} неинтересное.");
+}
