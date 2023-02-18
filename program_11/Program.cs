b174c8513a5991153f50d3f9f94b67e065a674b0
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 18:22:30 +0300

    Create program 11 (решение доп.задачи №1)

--- program_11/Program.cs
+++ program_11/Program.cs
@@ -7,5 +7,29 @@
 
 Console.Clear();
 Console.Write("Введите сумму вклада: ");
-int dep=Convert.ToInt32(Console.ReadLine());
+int dep = Convert.ToInt32(Console.ReadLine());
 
+while (dep <= 0)
+{
+  Console.WriteLine("Введенная сумма меньше либо равна 0.");
+  Console.Write("Введите другую сумму вклада: ");
+  dep = Convert.ToInt32(Console.ReadLine());
+}
+if (dep > 0 && dep < 100)
+{
+  var finalDep = dep * 1.05;
+  Console.WriteLine("\n" + "На ваш вклад будет начисленно 5%.");
+  Console.WriteLine($"Общая сумма с процентами: {finalDep:f2}.");
+}
+else if (dep >= 100 && dep <= 200)
+{
+  var finalDep = dep * 1.07;
+  Console.WriteLine("\n" + "На ваш вклад будет начисленно 7%.");
+  Console.WriteLine($"Общая сумма с процентами: {finalDep:f2}.");
+}
+else if (dep > 200)
+{
+  var finalDep = dep * 1.10;
+  Console.WriteLine("\n" + "На ваш вклад будет начисленно 10%.");
+  Console.WriteLine($"Общая сумма с процентами: {finalDep:f2}.");
+}
