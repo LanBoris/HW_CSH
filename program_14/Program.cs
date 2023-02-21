84f848c50c79fcb1b1393ed3e5c3285c50760abd
Boris Lanovenko <blanovenko@gmail.com>
2023-02-22 01:13:32 +0300

    Create program 14 (решение задачи №27)

--- program_14/Program.cs
+++ program_14/Program.cs
@@ -4,3 +4,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number<=0)
+{
+  Console.WriteLine("Вы ввели отрицательное число или 0.");
+  Console.Write("Введите новое число: ");
+  number = Convert.ToInt32(Console.ReadLine());
+}
+
+int summ = 0;
+int tempNumber = number;
+
+while (tempNumber > 0)
+{
+  summ += tempNumber % 10;
+  tempNumber /= 10;
+}
+
+Console.WriteLine("\n" + $"Сумма цифр числа {number} равна: {summ}");
