d48c477aa1a3b778d154f4a59b81195fd8b00d0d
Boris Lanovenko <blanovenko@gmail.com>
2023-02-25 01:08:12 +0300

    Create program 16 (решение задачи №34)

--- program_16/Program.cs
+++ program_16/Program.cs
@@ -5,3 +5,34 @@
 // количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+Console.Clear();
+
+void FillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(100, 1000);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  Console.WriteLine("\n" + $"Заданный массив [{String.Join("; ", array)}]" + "\n");
+}
+
+void CounterEven(int[] array, int size)
+{
+  int count = 0;
+  for (int i = 0; i < size; i++)
+  {
+    if (array[i] % 2 == 0) count++;
+  }
+  Console.WriteLine($"В заданном массиве количество четных чисел равно {count}." + "\n");
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+CounterEven(array, size);
