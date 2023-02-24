9756248127554f6f476153370252f6f36426cb67
Boris Lanovenko <blanovenko@gmail.com>
2023-02-25 02:23:58 +0300

    Create program 18 (решение задачи №38)

--- program_18/Program.cs
+++ program_18/Program.cs
@@ -4,3 +4,39 @@
 // и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+Console.Clear();
+
+void FillArray(double[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().NextDouble();
+    array[i] = Math.Round(array[i] * 100, 2);
+  }
+}
+
+void PrintArray(double[] array)
+{
+  Console.WriteLine("\n" + $"Заданный массив [{String.Join("; ", array)}]" + "\n");
+}
+
+void DiffMaxMin(double[] array)
+{
+  double max = Math.Round(array.Max(), 2);
+  double min = Math.Round(array.Min(), 2);
+  double diffMaxMin = Math.Round(max - min, 2);
+  Console.WriteLine($"Разница между максимальным: {max} и минимальным: {min} элементами массива равна: {diffMaxMin}" + "\n");
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+while (size <= 0)
+{
+  Console.Write("Размер массива не может быть отрицательным или нулевым."
+                  + "\n" + "Введите новый размер: ");
+  size = Convert.ToInt32(Console.ReadLine());
+}
+double[] array = new double[size];
+FillArray(array);
+PrintArray(array);
+DiffMaxMin(array);
