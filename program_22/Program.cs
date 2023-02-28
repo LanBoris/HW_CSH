89cdd04e66b4ca4e655ef348935994250441b531
Boris Lanovenko <blanovenko@gmail.com>
2023-03-01 02:41:57 +0300

    Create program 22 (решение задачи №42)

---
+++ program_22/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 42
+// Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+
+// Первый вариант решения (математический)
+Console.Write("Введите десятичное число для преобразования: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number<=0)
+{
+  Console.WriteLine("Вы ввели 0 или отрицательное число!" + "\n");
+  Console.Write("Введите десятичное число для преобразования: ");
+  number = Convert.ToInt32(Console.ReadLine());
+}
+int remnant = 0;
+string result = "";
+if (number == 0) result = "0";
+else
+{
+  while (number >= 1)
+  {
+    remnant = number % 2;
+    number = number / 2;
+    result = Convert.ToString(remnant) + result;
+  }
+}
+Console.WriteLine($"В двоичной системе счисления это: {result}");
+
+// Второй вариант решения (исследовательско-читерский)
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// while (number<=0)
+// {
+//   Console.WriteLine("Вы ввели 0 или отрицательное число!" + "\n");
+//   Console.Write("Введите десятичное число для преобразования: ");
+//   number = Convert.ToInt32(Console.ReadLine());
+// }
+// string result = Convert.ToString(number, 2);
+// Console.WriteLine($"В двоичной системе счисления это: {result}");
