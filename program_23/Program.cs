a269586f7813dfa28a975ab3799b8e60b08ffb55
Boris Lanovenko <blanovenko@gmail.com>
2023-03-03 23:31:39 +0300

    Create program 23 (решение задачи №47)

--- program_23/Program.cs
+++ program_23/Program.cs
@@ -8,3 +8,48 @@
 
 Console.Clear();
 
+double[,] GetMatrix(int rows, int columns)
+{
+  double[,] matr = new double[rows, columns];
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      matr[i, j] = Math.Round(((new Random().NextDouble())*(new Random().Next(-10,10))), 1);
+    }
+  }
+  return matr;
+}
+
+void PrintMatrix(double[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write(matr[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+int CheckNumber(int number)
+{
+  while (number <= 0)
+  {
+    Console.WriteLine("Вы ввели 0 или отрицательное число!" + "\n");
+    Console.Write("Введите снова: ");
+    number = Convert.ToInt32(Console.ReadLine());
+  }
+  return number;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+rows = CheckNumber(rows);
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+columns = CheckNumber(columns);
+double[,] matrix = GetMatrix(rows, columns);
+Console.WriteLine("\n" + $"Массив случайных вещественных чисел:");
+PrintMatrix(matrix);
