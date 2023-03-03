cb4e97480c22cee3e176a6efd271efbcb420f7d5
Boris Lanovenko <blanovenko@gmail.com>
2023-03-04 00:44:45 +0300

    Create program 24 (решение задачи №50)

--- program_24/Program.cs
+++ program_24/Program.cs
@@ -9,3 +9,64 @@
 
 Console.Clear();
 
+int[,] GetMatrix(int rows, int cols)
+{
+  int[,] matrix = new int[rows, cols];
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      matrix[i, j] = new Random().Next(1, 11);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+  Console.WriteLine("\n" + "В нашем массиве:");
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write(matr[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+int CheckNumber(int number)
+{
+  while (number < 0)
+  {
+    Console.WriteLine("Вы ввели отрицательное число!" + "\n");
+    Console.Write("Введите снова: ");
+    number = Convert.ToInt32(Console.ReadLine());
+  }
+  return number;
+}
+
+void CheckPosition(int[,] matr, int checkRow, int checkColumn)
+{
+  if ((checkRow < matr.GetLength(0)) && (checkColumn < matr.GetLength(1)))
+  {
+    Console.WriteLine($"В ячейке [{checkRow}, {checkColumn}] находится" 
+                      + $" элемент: {matr[checkRow, checkColumn]}");
+  }
+  else Console.WriteLine("Такой ячейки нет!");
+}
+
+int rows = new Random().Next(2, 6);
+int columns = new Random().Next(2, 6);
+int[,] matrix = GetMatrix(rows, columns);
+
+Console.WriteLine("Введите координаты ячейки.");
+Console.Write("Номер строки: ");
+int checkRow = Convert.ToInt32(Console.ReadLine());
+checkRow = CheckNumber(checkRow);
+Console.Write("Номер столбца:");
+int checkColumn = Convert.ToInt32(Console.ReadLine());
+checkColumn = CheckNumber(checkColumn);
+
+PrintMatrix(matrix);
+CheckPosition(matrix, checkRow, checkColumn);
