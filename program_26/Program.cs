eaabaed5425229b41b005cd61762289920a36bca
Boris Lanovenko <blanovenko@gmail.com>
2023-03-04 17:53:23 +0300

    Create program 26 change 1

--- program_26/Program.cs
+++ program_26/Program.cs
@@ -8,3 +8,51 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
 Console.Clear();
+
+int[,] GetMatrix(int rows, int cols, int minValue, int maxValue)
+{
+  int[,] matrix = new int[rows, cols];
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+  Console.WriteLine("\n" + "В нашем массиве:");
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write(matr[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+int CheckRowsColumns(int number)
+{
+  while (number < 0)
+  {
+    Console.WriteLine("Вы ввели отрицательное число!" + "\n");
+    Console.Write("Введите снова: ");
+    number = Convert.ToInt32(Console.ReadLine());
+  }
+  return number;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+rows = CheckRowsColumns(rows);
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+columns = CheckRowsColumns(columns);
+Console.Write("Введите минимальное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
