de6d33fc4b13ad2039e00970ca2e7293fd429867
Boris Lanovenko <blanovenko@gmail.com>
2023-03-09 18:46:31 +0300

    Create program 29 (решение задачи №58)

--- program_29/Program.cs
+++ program_29/Program.cs
@@ -9,3 +9,71 @@
 // 15 18
 
 Console.Clear();
+
+int rows = 2;
+int columns = 2;
+Console.Write("Введите минимальное значение интервала чисел: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение интервала чисел: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+int[,] matrixOne = FillMatrix(rows, columns, minValue, maxValue);
+int[,] matrixTwo = FillMatrix(rows, columns, minValue, maxValue);
+Console.WriteLine("Первая матрица:");
+PrintMatrix(matrixOne);
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(matrixTwo);
+Console.WriteLine("Результат перемножения матриц:");
+int[,] multiMatrix = FillMultiplyMatrix(matrixOne, matrixTwo);
+PrintMatrix(multiMatrix);
+
+
+int[,] FillMatrix(int rows, int cols, int minValue, int maxValue)
+{
+  int[,] matrix = new int[rows, cols];
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(int[,] inputMatrix)
+{
+  for (int i = 0; i < inputMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < inputMatrix.GetLength(1); j++)
+    {
+      Console.Write(inputMatrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+int[,] FillMultiplyMatrix(int[,] matrOne, int[,] matrTwo)
+{
+  int countRows = matrOne.GetLength(0);
+  int countCols = matrOne.GetLength(1);
+  int[,] multiplyMatrix = new int[countRows, countCols];
+  for (int i = 0; i < countRows - 1; i++)
+  {
+    for (int i1 = 1; i1 < countRows; i1++)
+    {
+      for (int j = 0; j < countCols - 1; j++)
+      {
+        for (int j1 = 1; j1 < countCols; j1++)
+        {
+          multiplyMatrix[i, j] = (matrOne[i, j] * matrTwo[i, j]) + (matrOne[i, j1] * matrTwo[i1, j]);
+          multiplyMatrix[i, j1] = (matrOne[i, j] * matrTwo[i, j1]) + (matrOne[i, j1] * matrTwo[i1, j1]);
+          multiplyMatrix[i1, j] = (matrOne[i1, j] * matrTwo[i, j]) + (matrOne[i1, j1] * matrTwo[i1, j]);
+          multiplyMatrix[i1, j1] = (matrOne[i1, j] * matrTwo[i, j1]) + (matrOne[i1, j1] * matrTwo[i1, j1]);
+        }
+      }
+    }
+  }
+  return multiplyMatrix;
+}
