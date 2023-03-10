81b2a73bc8592194c3d8afd6747b5e5f846e5719
Boris Lanovenko <blanovenko@gmail.com>
2023-03-10 13:22:37 +0300

    Create program 31

--- program_31/Program.cs
+++ program_31/Program.cs
@@ -7,3 +7,52 @@
 // 10 09 08 07
 
 Console.Clear();
+
+int rows = 4;
+int columhs = 4;
+int[,] matrix = CreateSnakeArray(rows, columhs);
+PrintMatrix(matrix);
+
+/// <summary>
+/// Метод, который заполняет двумерный массив по спирали
+/// </summary>
+/// <param name="rows">Кол-во строк массива</param>
+/// <param name="cols">Кол-во столбцов массива</param>
+/// <returns>Заполненный двумерный массив</returns>
+int[,] CreateSnakeArray(int rows, int cols)
+{
+  int[,] matrix = new int[rows, cols];
+  int row = 0, col = 0, dx = 1, dy = 0, dirChanges = 0, gran = cols;
+
+  for (int i = 0; i < matrix.Length; i++)
+  {
+    matrix[col, row] = i + 1;
+    if (--gran == 0)
+    {
+      gran = cols * (dirChanges % 2) + rows * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+      int temp = dx;
+      dx = -dy;
+      dy = temp;
+      dirChanges++;
+    }
+    row += dx;
+    col += dy;
+  }
+  return matrix;
+}
+
+/// <summary>
+/// Метод выводящий двумерный массив на экран
+/// </summary>
+/// <param name="inputMatrix">Входящий массив, который надо вывести на экран</param>
+void PrintMatrix(int[,] inputMatrix)
+{
+  for (int i = 0; i < inputMatrix.GetLength(1); i++)
+  {
+    for (int j = 0; j < inputMatrix.GetLength(0); j++)
+    {
+      Console.Write(inputMatrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
